8bc9ac049b69fc6492bc73eaf1a24ea893cf6e46
vicsanch <vicsanch@student.42madrid.com>
2026-09-25 19:39:32 +0200

    faltaba el ft_

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,13 +6,13 @@
 /*   By: vicsanch <vicsanch@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/25 19:18:19 by vicsanch          #+#    #+#             */
-/*   Updated: 2026/09/25 19:34:00 by vicsanch         ###   ########.fr       */
+/*   Updated: 2026/09/25 19:39:15 by vicsanch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*strchr(const char *s, int c)
+char	*ft_strchr(const char *s, int c)
 {
 	int	i;
 
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: vicsanch <vicsanch@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/24 17:52:05 by vicsanch          #+#    #+#             */
-/*   Updated: 2026/09/25 19:34:12 by vicsanch         ###   ########.fr       */
+/*   Updated: 2026/09/25 19:39:20 by vicsanch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,6 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
-char	*strchr(const char *s, int c);
+char	*ft_strchr(const char *s, int c);
 
 #endif
