1ab3cd1475abcf03ecdb6070aa97a1ada1637cb3
vicsanch <vicsanch@student.42madrid.com>
2026-09-25 19:16:37 +0200

    strlcopy hecho

---
+++ ft_strlcpy.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vicsanch <vicsanch@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/25 17:38:14 by vicsanch          #+#    #+#             */
+/*   Updated: 2026/09/25 19:07:48 by vicsanch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+
+	if (size > 0)
+	{
+		i = 0;
+		while (src[i] != 0 && i < (size - 1))
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
+	}
+	return (ft_strlen(src));
+}
