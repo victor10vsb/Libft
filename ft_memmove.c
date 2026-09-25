5e0fdbb2ed50641a7be509b8d619cc6806464037
vicsanch <vicsanch@student.42madrid.com>
2026-09-25 17:37:00 +0200

    memmove.c hecho

--- ft_memmove.c
+++ ft_memmove.c
@@ -3,11 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vicsanch <vicsanch@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: vicsanch <vicsanch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/24 19:28:34 by vicsanch          #+#    #+#             */
-/*   Updated: 2026/09/24 19:28:57 by vicsanch         ###   ########.fr       */
+/*   Updated: 2026/09/25 17:33:13 by vicsanch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	unsigned char		*d;
+	const unsigned char	*s;
+	size_t				i;
+
+	d = (unsigned char *)dest;
+	s = (const unsigned char *)src;
+	if (d < s)
+	{
+		i = 0;
+		while (i < n)
+		{
+			d[i] = s[i];
+			i++;
+		}
+	}
+	else
+	{
+		i = n;
+		while (i > 0)
+		{
+			i--;
+			d[i] = s[i];
+		}
+	}
+	return (dest);
+}
