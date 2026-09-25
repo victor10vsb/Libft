b17401333017149afdd87670a9891d95c0c39b8d
vicsanch <vicsanch@student.42madrid.com>
2026-09-25 19:17:56 +0200

    cabecera actualizada hasta tolower

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: vicsanch <vicsanch@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/24 17:52:05 by vicsanch          #+#    #+#             */
-/*   Updated: 2026/09/24 19:28:19 by vicsanch         ###   ########.fr       */
+/*   Updated: 2026/09/25 19:17:38 by vicsanch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,10 @@ size_t	ft_strlen(const char *s);
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memset(void *b, int c, size_t len);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+int	ft_toupper(int c);
+int	ft_tolower(int c);
 
 #endif
